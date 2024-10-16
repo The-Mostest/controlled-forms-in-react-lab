292f7a7a26df7bb8e4db61000aaa912dce3a5dac
the-mostest <fbmcdougall@gmail.com>
2024-10-16 12:47:55 +0100

    Setup Code

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,45 @@
 // src/App.jsx
+import { useState } from 'react';
 import './App.css'
+import Bookshelf from './component/Bookshelf.jsx';
+
+
+
 const App = () => {
-  return <h1>Hello world!</h1>;
+  return (
+<>
+
+<h1> My Bookshelf</h1>
+<Bookshelf />
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    </>
+  )
 };
 
 export default App;
